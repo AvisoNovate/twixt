39c237ebb45cf5694a65a1e75968226b31b71619
Howard M. Lewis Ship <hlship@gmail.com>
2013-08-28 18:35:42 +0100

    Advance version number, update less4j to 1.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.aviso/twixt "0.1.1"
+(defproject io.aviso/twixt "0.1.2"
   :description "An extensible asset pipeline for Clojure web applications"
   :url "https://github.com/AvisoNovate/twixt"
   :license {:name "Apache Sofware Licencse 2.0"
@@ -7,7 +7,7 @@
                  [org.clojure/tools.logging "0.2.6"]
                  [ring/ring-core "1.1.8"]
                  [org.mozilla/rhino "1.7R4"]
-                 [com.github.sommeri/less4j "1.0.4"]
+                 [com.github.sommeri/less4j "1.1.1"]
                  [de.neuland/jade4j "0.3.12"]]
   :repositories [["jade4j" "https://raw.github.com/neuland/jade4j/master/releases"]]
   :profiles {:dev {:dependencies [[log4j "1.2.17"]]}})
