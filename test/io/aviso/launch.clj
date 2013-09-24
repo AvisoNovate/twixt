22164a6a380fd1d7f81c545f2b4a16924f1c93ed
Howard M. Lewis Ship <hlship@gmail.com>
2013-09-24 19:56:03 +0100

    Change the demo page for excepton reporting to be more involved

--- test/io/aviso/launch.clj
+++ test/io/aviso/launch.clj
@@ -1,21 +1,18 @@
 (ns io.aviso.launch
   (use io.aviso.twixt 
        [io.aviso.twixt exceptions tracker]       
-       ring.adapter.jetty)
-  (import [java.sql SQLException]))
+       ring.adapter.jetty))
 
-(defn handler [request]
-  (trace "Invoking handler (that throws exceptions)"
-         (throw 
-           (->>
-             (SQLException. "Inner Exception" "SQL-STATE", 999)
-             (RuntimeException. "Middle Exception")
-             (IllegalArgumentException. "Outer Exception")))))
+(defn make-handler [twixt] 
+  (fn [request]
+    (trace "Invoking handler (that throws exceptions)"
+           ;; This will fail at some depth:
+           (get-asset-uri twixt "invalid-coffeescript.coffee"))))
 
 (defn app []
   (let [twixt (new-twixt {:development-mode true})]
     (->
-      handler
+      (make-handler twixt)
       (wrap-with-twixt twixt)
       (wrap-with-exception-reporting twixt))))
 
