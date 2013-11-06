8f4fbf2b7d5d0b6587f5cf49392ae68fb61d79ef
Howard M. Lewis Ship <hlship@gmail.com>
2013-11-06 12:05:44 -0800

    Configure Codox

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "An extensible asset pipeline for Clojure web applications"
   :url "https://github.com/AvisoNovate/twixt"
   :license {:name "Apache Sofware Licencse 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+            :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [ring/ring-core "1.2.0"]
@@ -12,10 +12,7 @@
                  [io.aviso/pretty "0.1.4"]
                  [hiccup "1.0.4"]]
   :repositories [["jade4j" "https://raw.github.com/neuland/jade4j/master/releases"]]
-  :repl-options {
-                 :init-ns io.aviso.launch
-                 :port 4001}
-  ;; The Sublime Text nREPL plugin is out of date, so...
-  :repl-port 4001
+  :codox {:src-dir-uri               "https://github.com/AvisoNovate/twixt/blob/master/"
+          :src-linenum-anchor-prefix "L"}
   :profiles {:dev {:dependencies [[log4j "1.2.17"]
                                   [ring/ring-jetty-adapter "1.2.0"]]}})
