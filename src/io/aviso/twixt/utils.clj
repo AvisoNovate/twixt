c78c873114516b9455ec4d81b9e151fd6a4b0464
Howard M. Lewis Ship <hlship@gmail.com>
2013-11-08 09:55:29 -0800

    Remove unused code

--- src/io/aviso/twixt/utils.clj
+++ src/io/aviso/twixt/utils.clj
@@ -9,30 +9,6 @@
             [clojure.string :as str]
             [clojure.pprint :as pp]))
 
-(defn transform-values
-  "Transforms a map by passing each value through a provided function."
-  [m f]
-  (into {} (map (fn [[k v]] [k (f v)]) m)))
-
-(declare merge-maps-recursively)
-
-(defn- merge-values [l r]
-  (cond
-    ;; We know how to merge two maps together:
-    (and (map? l) (map? r)) (merge-maps-recursively l r)
-    ;; and how to merge two seqs  together
-    (and (seq? l) (seq? r)) (concat l r)
-    ;; In any other case the right (later) value replaces the left (earlier) value
-    :else r))
-
-(defn merge-maps-recursively
-  "Merges any number of maps together, recursively. When merging values:
-  - two maps are merged, recursively
-  - two seqs are concatinated
-  - otherwise, the 'right' value overwrites the 'left' value"
-  [& maps]
-  (apply merge-with merge-values maps))
-
 (defn as-string
   "Converts a source (compatible with clojure.java.io/IOFactory) into a String using the provided encoding.
 
--- test/io/aviso/utils_test.clj
+++ test/io/aviso/utils_test.clj
@@ -2,23 +2,6 @@
   (:use io.aviso.twixt.utils
         clojure.test))
 
-(deftest map-merging
-
-  (is (=
-        (merge-maps-recursively {:a 1} {:b 2} {:a 3 :c 3})
-        {:a 3 :b 2 :c 3}))
-
-  (deftest merge-map-values
-    (is (=
-          (merge-maps-recursively {:a {:b 1}} {:a {:b 2 :c 2}})
-          {:a {:b 2 :c 2}})))
-
-  (deftest merge-seq-values
-    (is (=
-          (merge-maps-recursively {:a [1]} {:a [2] :c [3 4]} {:c [5 6]})
-          {:a [1 2]
-           :c [3 4 5 6]}))))
-
 (deftest relative-paths
 
   (are [start relative expected] (= (compute-relative-path start relative) expected)
