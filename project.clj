b8bcb7491312b689c2224b620ac50102206c0295
Howard M. Lewis Ship <hlship@gmail.com>
2014-03-07 15:11:07 -0800

    Setup version number for release

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject io.aviso/twixt "0.1.10-SNAPSHOT"
+(defproject io.aviso/twixt "0.1.10"
   :description "An extensible asset pipeline for Clojure web applications"
   :url "https://github.com/AvisoNovate/twixt"
   :license {:name "Apache Sofware Licencse 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [io.aviso/pretty "0.1.10-SNAPSHOT"]
+                 [io.aviso/pretty "0.1.10"]
                  [io.aviso/tracker "0.1.0"]
                  [ring/ring-core "1.2.1"]
                  [org.mozilla/rhino "1.7R4"]
