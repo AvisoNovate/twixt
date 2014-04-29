51ce959e49b805680cb4214ad5b22fdf27280ad7
Howard M. Lewis Ship <hlship@gmail.com>
2014-04-29 09:47:17 -0700

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject io.aviso/twixt "0.1.11"
+(defproject io.aviso/twixt "0.1.12-SNAPSHOT"
   :description "An extensible asset pipeline for Clojure web applications"
   :url "https://github.com/AvisoNovate/twixt"
   :license {:name "Apache Sofware Licencse 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [io.aviso/pretty "0.1.10"]
                  [io.aviso/tracker "0.1.0"]
-                 [ring/ring-core "1.2.1"]
+                 [ring/ring-core "1.2.2"]
                  [org.mozilla/rhino "1.7R4"]
-                 [com.github.sommeri/less4j "1.4.0"]
+                 [com.github.sommeri/less4j "1.5.3"]
                  [de.neuland-bfi/jade4j "0.4.0"]
                  [hiccup "1.0.4"]]
   :codox {:src-dir-uri               "https://github.com/AvisoNovate/twixt/blob/master/"
