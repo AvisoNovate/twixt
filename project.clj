41314990c056c40936c05630530e12a3c83e3115
Howard M. Lewis Ship <hlship@gmail.com>
2014-10-24 08:43:02 -0700

    Update some dependencies

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [io.aviso/pretty "0.1.12"]
-                 [io.aviso/tracker "0.1.1"]
+                 [io.aviso/tracker "0.1.2"]
                  [ring/ring-core "1.3.1"]
                  [com.google.javascript/closure-compiler "v20140814"]
                  [org.mozilla/rhino "1.7R4"]
-                 [com.github.sommeri/less4j "1.8.0"]
+                 [com.github.sommeri/less4j "1.8.3"]
                  [de.neuland-bfi/jade4j "0.4.0"]
                  [hiccup "1.0.5"]]
   :test-paths ["spec"]
