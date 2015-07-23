01ad8cbfcb55319c061b5e9e3f455bf155451897
Howard M. Lewis Ship <hlship@gmail.com>
2015-07-23 11:06:34 -0600

    Split out example webapp from user namespace

---
+++ dev-resources/example.clj
@@ -0,0 +1,25 @@
+(ns example
+  (:use ring.adapter.jetty
+        io.aviso.twixt.startup)
+  (:require [io.aviso.tracker :as t]
+            [io.aviso.twixt :refer [get-asset-uris default-options]]))
+
+(defn handler
+  [request]
+  (t/track "Invoking handler (that throws exceptions)"
+           (if (= (:uri request) "/fail")
+             ;; This will fail at some depth:
+             (doall
+               (get-asset-uris (:twixt request) "invalid-coffeescript.coffee")))))
+
+(defn app
+  []
+  (wrap-with-twixt handler default-options true))
+
+(defn launch
+  []
+  (run-jetty (app) {:port 8888 :join? false}))
+
+(defn shutdown
+  [server]
+  (.stop server))
--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -1,12 +1,8 @@
 (ns user
   (:use speclj.config
-        io.aviso.tracker
         io.aviso.repl
         clojure.pprint
-        [io.aviso.twixt :only [get-asset-uris default-options]]
-        io.aviso.exception
-        io.aviso.twixt.startup
-        ring.adapter.jetty))
+        io.aviso.exception))
 
 (install-pretty-exceptions)
 
@@ -16,22 +12,3 @@
 
 (alter-var-root #'default-config assoc :color true :reporters ["documentation"])
 
-(defn handler
-  [request]
-  (track "Invoking handler (that throws exceptions)"
-         (if (= (:uri request) "/fail")
-           ;; This will fail at some depth:
-           (doall
-             (get-asset-uris (:twixt request) "invalid-coffeescript.coffee")))))
-
-(defn app
-  []
-  (wrap-with-twixt handler default-options true))
-
-(defn launch
-  []
-  (run-jetty (app) {:port 8888 :join? false}))
-
-(defn shutdown
-  [server]
-  (.stop server))
