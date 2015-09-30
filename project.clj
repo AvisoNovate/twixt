eef6f2ddfe23bec5e6d8659977460f8b53a0adee
Nicolae Claudius <claudius.nicolae@gmail.com>
2015-09-30 15:44:18 +0300

    Update a bunch of dependencies
    * 1.6 profile for backwards compatibility

--- project.clj
+++ project.clj
@@ -3,19 +3,19 @@
   :url "https://github.com/AvisoNovate/twixt"
   :license {:name "Apache Sofware Licencse 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [io.aviso/pretty "0.1.19"]
                  [io.aviso/tracker "0.1.7"]
-                 [medley "0.6.0"]
+                 [medley "0.7.0"]
                  [ring/ring-core "1.4.0"]
                  [com.google.javascript/closure-compiler "v20150729"]
                  [org.mozilla/rhino "1.7.7"]
                  [com.github.sommeri/less4j "1.14.0"]
                  [de.neuland-bfi/jade4j "0.4.3"]
-                 [prismatic/schema "0.4.4"]
+                 [prismatic/schema "1.0.1"]
                  [hiccup "1.0.5"]
                  [org.webjars/bootstrap "3.3.5"]
-                 [org.webjars/webjars-locator-core "0.27"]]
+                 [org.webjars/webjars-locator-core "0.28"]]
   ;; We keep a local copy of YUICompressor's CSSCompressor; we don't want the rest of the dependency
   ;; since it does evil things to patch Rhino to support JavaScript compression.
   :java-source-paths ["java"]
@@ -36,6 +36,9 @@
           :src-linenum-anchor-prefix "L"
           :defaults                  {:doc/format :markdown}}
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.3"]
-                                  [speclj "3.2.0"]
+                                  [speclj "3.3.1"]
                                   [ring/ring-jetty-adapter "1.4.0"]]
-                   :jvm-opts     ["-Xmx1g"]}})
+                   :jvm-opts     ["-Xmx1g"]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]
+                                  [speclj "3.2.0"]
+                                  [medley "0.6.0"]]}})
